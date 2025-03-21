bdb34c43dd5f31e6689a520546010c02a7970a2f
PaulaBGM <borrasgarciap@gmail.com>
2025-03-21 13:23:48 +0100

    maquina de estados arreglada

--- Assets/Scripts/AI/EnemyBehavior.cs
+++ Assets/Scripts/AI/EnemyBehavior.cs
@@ -13,7 +13,6 @@ public enum EnemyState
     Attack,
     Jump,
 }
-
 [Serializable, RequireComponent(typeof(SphereCollider))]
 public class EnemyBehavior : MonoBehaviour
 {
@@ -26,12 +25,14 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] private AIBase[] aiStates;
     [SerializeField] private float timeBetweenAttacks = 2.5f;
     [SerializeField] private float strongAttackChance = 0.3f;
-    [SerializeField] private float runSpeed = 5f;
-    [SerializeField] private float normalSpeed = 3f;
+    [SerializeField] private float wanderingTime = 5f; // Tiempo en segundos antes de cambiar de estado
+    private float currentWanderingTime = 0f; // Tiempo transcurrido en estado Wandering
+    private float runSpeed = 5f;
+    private float normalSpeed = 3f;
+    private float idleSpeed = 0f;
     private NavMeshAgent agent;
     private Animator animator;
     private float timeNextAttack;
-    private AIEnemyVision checkVision;
 
     void Start()
     {
@@ -39,11 +40,11 @@ public class EnemyBehavior : MonoBehaviour
         aiStates = GetComponents<AIBase>();
         sphereCollider = GetComponent<SphereCollider>();
         animator = GetComponent<Animator>();
-        checkVision = GetComponent<AIEnemyVision>();
-
-        // Set initial state
-        ChangeState(EnemyState.FollowingPath);
+        sphereCollider.radius = detectionDistance;
+        state = EnemyState.Wandering;
+        
     }
+
 
     void Update()
     {
@@ -54,12 +55,10 @@ public class EnemyBehavior : MonoBehaviour
         else if (state == EnemyState.FollowingPlayer)
         {
             UpdateFollowingPlayer();
-            agent.speed = runSpeed;
         }
         else if (state == EnemyState.FollowingPath)
         {
             UpdateFollowingPath();
-            agent.speed = normalSpeed;
         }
         else if (state == EnemyState.Attack)
         {
@@ -70,89 +69,97 @@ public class EnemyBehavior : MonoBehaviour
             UpdateJump();
         }
     }
-
     private void UpdateWandering()
     {
-        if (checkVision.CheckVisionToPlayer())
+        if (!PlayerIsOnRange(detectionDistance))
         {
-            ChangeState(EnemyState.FollowingPlayer);
-        }
-        animator.SetFloat("speed", agent.speed);
-    }
+            agent.speed = idleSpeed;
+            // Contamos el tiempo que lleva vagando
+            currentWanderingTime += Time.deltaTime;
 
-    private void UpdateFollowingPlayer()
-    {
-        if (!checkVision.CheckVisionToPlayer()) return;
-        // Si está a la distancia de ataque, cambia al estado de ataque.
-        if (checkVision.CheckAttackDistance(attackDistance))
-        {
-            ChangeState(EnemyState.Attack);
+            // Si supera el tiempo definido, cambia a FollowingPath
+            if (currentWanderingTime >= wanderingTime)
+            {
+                agent.speed = normalSpeed;
+                ChangeState(EnemyState.FollowingPath);
+                currentWanderingTime = 0f; // Reseteamos el contador
+            }
+
+            return;
         }
 
-        // Si el jugador ya no es visible, pasa al estado de FollowingPath.
-        if (checkVision.CheckVisionToPlayer())
-        {
-            Debug.Log("Jugador a la vista: RESET PATH");
-            agent.ResetPath();
-            ChangeState(EnemyState.FollowingPath);
-        }
-       
-
+        ChangeState(EnemyState.FollowingPlayer);
+        currentWanderingTime = 0f;
+        
+        animator.SetFloat("zSpeed", agent.speed);
     }
 
     private void UpdateFollowingPath()
     {
-        // Si el jugador es visible, pasa al estado de FollowingPlayer.
-        if (!checkVision.CheckVisionToPlayer()) return;
-        ChangeState(EnemyState.FollowingPlayer);
-        
-    }
-
-    private void UpdateAttack()
-    {
-        if (!checkVision.CheckAttackDistance(attackDistance))
+        if (!PlayerIsOnRange(detectionDistance)) return;
+        if (PlayerIsOnRange(attackDistance))
         {
-            ChangeState(EnemyState.FollowingPlayer); // Si está fuera del rango de ataque, sigue al jugador.
+            ChangeState(EnemyState.Attack);
         }
         else
         {
-            // Ataque al jugador.
+            agent.ResetPath();
+            ChangeState(EnemyState.FollowingPlayer);
+        }
+        
+        animator.SetFloat("zSpeed", agent.speed);
+    }
+
+    private void UpdateFollowingPlayer()
+    {
+        agent.speed = runSpeed;
+        if (PlayerIsOnRange(detectionDistance)) return;
+        ChangeState(EnemyState.Wandering);
+    }
+    private void UpdateAttack()
+    {
+        if (!PlayerIsOnRange(attackDistance))
+        {
+            ChangeState(EnemyState.FollowingPlayer);
+        }
+        else
+        {
             transform.LookAt(target.position);
             timeNextAttack -= Time.deltaTime;
-
             if (timeNextAttack <= 0)
             {
                 if (Random.Range(0.0f, 1.0f) < strongAttackChance)
                 {
-                    // Animación de ataque fuerte.
+                    //animacion strong attack
                 }
                 else
                 {
-                    // Animación de ataque normal.
+                    //animacion normal attack
                 }
                 timeNextAttack = timeBetweenAttacks;
             }
         }
     }
-
     private void UpdateJump()
     {
-        // Aquí podrías implementar el comportamiento para saltar, si es necesario.
-        if (checkVision.CheckVisionToPlayer())
-        {
-            ChangeState(EnemyState.FollowingPlayer);
-        }
+        if (!PlayerIsOnRange(detectionDistance)) return;
+        ChangeState(EnemyState.FollowingPlayer);
     }
 
-    public void ChangeState(EnemyState newState)
+    private void ChangeState(EnemyState newState)
     {
         state = newState;
 
-        // Activa el comportamiento correspondiente al nuevo estado.
         for (int i = 0; i < aiStates.Length; i++)
         {
             aiStates[i].enabled = i == (int)state;
         }
+    }
+
+    private bool PlayerIsOnRange(float range)
+    {
+        var sqrDistance = (target.position - transform.position).sqrMagnitude;
+        return sqrDistance <= Mathf.Pow(range, 2);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -160,5 +167,5 @@ public class EnemyBehavior : MonoBehaviour
         if (other.GetComponent<ITargeteable>() == null) return;
         target = other.transform;
     }
-}
 
+}
